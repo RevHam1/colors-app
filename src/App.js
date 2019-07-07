2d36625ada08b1772fe90ed111b9f7f7e43d05bf
RevHam1 <oldhamtyrone@yahoo.com>
2019-07-06 23:30:46 -0500

    Finish MiniPalette delete

--- src/App.js
+++ src/App.js
@@ -20,6 +20,13 @@ class App extends Component {
     });
   };
 
+  deletePalette = id => {
+    this.setState(
+      st => ({ palettes: st.palettes.filter(palette => palette.id !== id) }),
+      this.syncLocalStorage
+    );
+  };
+
   savePalette = newPalette => {
     this.setState(
       { palettes: [...this.state.palettes, newPalette] },
@@ -65,7 +72,11 @@ class App extends Component {
           exact
           path="/"
           render={routeProps => (
-            <PaletteList palettes={this.state.palettes} {...routeProps} />
+            <PaletteList
+              palettes={this.state.palettes}
+              deletePalette={this.deletePalette}
+              {...routeProps}
+            />
           )}
         />
         <Route
